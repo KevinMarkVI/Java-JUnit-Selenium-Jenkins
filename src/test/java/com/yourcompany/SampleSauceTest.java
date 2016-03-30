b9de2a1db212d07f242508a6daddc7caf504168f
Kevin Berg <kevinberg@Kevins-MacBook-Pro.local>
2016-03-30 14:24:38 -0700

    Add JSON Functionality

--- src/test/java/com/yourcompany/SampleSauceTest.java
+++ src/test/java/com/yourcompany/SampleSauceTest.java
@@ -1,28 +1,39 @@
 package com.yourcompany;
 
 import com.saucelabs.common.SauceOnDemandAuthentication;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TestName;
+import org.junit.runner.RunWith;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
+
+import com.saucelabs.junit.ConcurrentParameterized;
 import com.saucelabs.junit.SauceOnDemandTestWatcher;
 
 import java.net.URL;
+import java.util.LinkedList;
+
 import static org.junit.Assert.*;
+
 import com.saucelabs.common.SauceOnDemandSessionIdProvider;
 
+
+@RunWith(ConcurrentParameterized.class)
 public class SampleSauceTest implements SauceOnDemandSessionIdProvider {
 
     public String username = System.getenv("SAUCE_USERNAME");
     public String accesskey = System.getenv("SAUCE_ACCESS_KEY");
-    public String os = System.getenv("SELENIUM_PLATFORM");
-    public String browserName = System.getenv("SELENIUM_BROWSER");
-    public String version = System.getenv("SELENIUM_VERSION");
 
     /**
      * Constructs a {@link SauceOnDemandAuthentication} instance using the supplied user name/access key.  To use the authentication
@@ -38,16 +49,79 @@ public class SampleSauceTest implements SauceOnDemandSessionIdProvider {
 
     @Rule public TestName name = new TestName() {
         public String getMethodName() {
-        		return String.format("%s : (%s %s %s)", super.getMethodName(), os, browserName, version);
+        		return String.format("%s : (%s %s %s)", super.getMethodName(), os, browser, version);
         };
     };
 
+    /**
+     * Represents the browser to be used as part of the test run.
+     */
+    private String browser;
+    /**
+     * Represents the operating system to be used as part of the test run.
+     */
+    private String os;
+    /**
+     * Represents the version of the browser to be used as part of the test run.
+     */
+    private String version;
+    /**
+     * Represents the deviceName of mobile device
+     */
+    private String deviceName;
+    /**
+     * Represents the device-orientation of mobile device
+     */
+    private String deviceOrientation;
+    /**
+     * Instance variable which contains the Sauce Job Id.
+     */
     private String sessionId;
 
     /**
      * The {@link WebDriver} instance which is used to perform browser interactions with.
      */
     private WebDriver driver;
+
+    /**
+     * Constructs a new instance of the test.  The constructor requires three string parameters, which represent the operating
+     * system, version and browser to be used when launching a Sauce VM.  The order of the parameters should be the same
+     * as that of the elements within the {@link #browsersStrings()} method.
+     * @param os
+     * @param version
+     * @param browser
+     * @param deviceName
+     * @param deviceOrientation
+     */
+
+    public SampleSauceTest(String os, String version, String browser, String deviceName, String deviceOrientation) {
+        super();
+        this.os = os;
+        this.version = version;
+        this.browser = browser;
+        this.deviceName = deviceName;
+        this.deviceOrientation = deviceOrientation;
+    }
+
+    /**
+     * @return a LinkedList containing String arrays representing the browser combinations the test should be run against. The values
+     * in the String array are used as part of the invocation of the test constructor
+     * @throws JSONException 
+     */
+    @ConcurrentParameterized.Parameters
+    public static LinkedList browsersStrings() throws JSONException {
+        LinkedList browsers = new LinkedList();
+        String browsersJSONArrayString = System.getenv("SAUCE_ONDEMAND_BROWSERS");
+        JSONArray browsersJSONArrayObj = new JSONArray(browsersJSONArrayString);
+        
+        for (int i=0; i<browsersJSONArrayObj.length(); i++) {
+            JSONObject browserObj = (JSONObject)browsersJSONArrayObj.getJSONObject(i);
+            browsers.add(new String[]{browserObj.getString("os"), browserObj.getString("browser-version"), browserObj.getString("browser"), null, null});
+        }
+
+        return browsers;
+    }
+
 
     /**
      * Constructs a new {@link RemoteWebDriver} instance which is configured to use the capabilities defined by the {@link #browser},
@@ -60,8 +134,11 @@ public class SampleSauceTest implements SauceOnDemandSessionIdProvider {
     public void setUp() throws Exception {
         DesiredCapabilities capabilities = new DesiredCapabilities();
 
-        capabilities.setCapability(CapabilityType.BROWSER_NAME, browserName);
-        capabilities.setCapability(CapabilityType.VERSION, version);
+        if (browser != null) capabilities.setCapability(CapabilityType.BROWSER_NAME, browser);
+        if (version != null) capabilities.setCapability(CapabilityType.VERSION, version);
+        if (deviceName != null) capabilities.setCapability("deviceName", deviceName);
+        if (deviceOrientation != null) capabilities.setCapability("device-orientation", deviceOrientation);
+
         capabilities.setCapability(CapabilityType.PLATFORM, os);
 
         String methodName = name.getMethodName();
@@ -82,12 +159,10 @@ public class SampleSauceTest implements SauceOnDemandSessionIdProvider {
      * @throws Exception
      */
     @Test
-    public void verifyEmailInputTest() throws Exception {        
-        String expectedTitle = "I am a page title - Sauce Labs";
-        
+    public void verifyBelkHompage() throws Exception {
         driver.get("https://saucelabs.com/test/guinea-pig");
-        assertEquals(driver.getTitle(), expectedTitle);
- 
+
+        assertTrue(driver.getTitle().equals("I am a page title - Sauce Labs"));
     }
 
     /**
